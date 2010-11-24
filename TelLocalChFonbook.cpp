35d3f8dca9a1296bf970e336b6aa6f2e25d3e585
Joachim Wilke <git@joachim-wilke.de>
2010-11-24 21:05:46 +0100

    Re-apply three lost commits
    - Fix resolving calls from unkown caller
    - Code cleanup in Listener, OertlichesFonbook and Nummerzoeker
    - Add tel.local.ch phone book

--- TelLocalChFonbook.cpp
+++ TelLocalChFonbook.cpp
@@ -2,6 +2,7 @@
  * libfritz++
  *
  * Copyright (C) 2007-2010 Joachim Wilke <libfritz@joachim-wilke.de>
+ * TelLocalChFonbook created by Christian Richter <cr@crichter.net>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,45 +23,46 @@
 
 #include <unistd.h>
 #include <TcpClient++.h>
-#include "OertlichesFonbook.h"
+#include "TelLocalChFonbook.h"
 #include "Config.h"
 #include "Tools.h"
 
 namespace fritz{
 
-OertlichesFonbook::OertlichesFonbook()
+TelLocalChFonbook::TelLocalChFonbook()
 {
-	title = I18N_NOOP("das-oertliche.de");
-	techId = "OERT";
+	title = I18N_NOOP("tel.local.ch");
+	techId = "LOCCH";
 	displayable = false;
 }
 
-OertlichesFonbook::~OertlichesFonbook()
+TelLocalChFonbook::~TelLocalChFonbook()
 {
 }
 
-bool OertlichesFonbook::Initialize() {
+bool TelLocalChFonbook::Initialize() {
 	setInitialized(true);
 	return true;
 }
 
-Fonbook::sResolveResult OertlichesFonbook::ResolveToName(std::string number) {
-	Fonbook::sResolveResult result;
+TelLocalChFonbook::sResolveResult TelLocalChFonbook::ResolveToName(std::string number) {
+	TelLocalChFonbook::sResolveResult result;
 	result.name = number;
 	result.type = FonbookEntry::TYPE_NONE;
 
-	// resolve only (german) phone numbers
-	if (number.length() == 0 || Tools::NormalizeNumber(number).find("0049") != 0)
+	// resolve only (swiss) phone numbers
+	if (number.length() == 0 || Tools::NormalizeNumber(number).find("0041") != 0)
 		return result;
-
+	
 	std::string msg;
 	std::string name;
 	try {
-		DBG("sending reverse lookup request for " << (gConfig->logPersonalInfo()? Tools::NormalizeNumber(number) : HIDDEN) << " to www.dasoertliche.de");
-		std::string host = "www.dasoertliche.de";
+		DBG("sending reverse lookup request for " << Tools::NormalizeNumber(number) << " to tel.local.ch");
+		std::string host = "tel.local.ch";
 		tcpclient::HttpClient tc(host);
 		tc << tcpclient::get
-		   << "/Controller?topKw=0&form_name=search_nat&context=0&choose=true&page=0&rci=yes&action=43&kw=" << Tools::NormalizeNumber(number)
+		   << "/de/q/" <<  Tools::NormalizeNumber(number)
+		   << ".html"
 		   << "\nAccept-Charset: ISO-8859-1\nUser-Agent: Lynx/2.8.5"
 		   << std::flush;
 		tc >> msg;
@@ -69,22 +71,24 @@ Fonbook::sResolveResult OertlichesFonbook::ResolveToName(std::string number) {
 		return result;
 	}
 	// parse answer
-	size_t start = msg.find("class=\"preview\">");
+	size_t start = msg.find("<h2");
+
 	if (start == std::string::npos) {
 		INF("no entry found.");
 		return result;
 	}
 	// add the length of search pattern
-	start += 16;
+	start += 15;
 
-	size_t stop  = msg.find("<", start);
+	size_t stop  = msg.find("</h2>", start);
 	name = msg.substr(start, stop - start);
+	
 	// convert the string from latin1 to current system character table
 	CharSetConv *conv = new CharSetConv("ISO-8859-1", CharSetConv::SystemCharacterTable());
 	const char *s_converted = conv->Convert(name.c_str());
 	name = s_converted;
 	delete (conv);
-	INF("resolves to " << (gConfig->logPersonalInfo() ? name.c_str() : HIDDEN));
+	INF("resolves to " << name.c_str());
 	result.name = name;
 	return result;
 }
