jowi24/libfritzpp

commit 69a43bd7f61d6feaf6d8c69b4f461fd191890d37

Make Fonbook::GetFonbookSize() more robust
- If not initialized, always return 0 and not the
  current intermediate state

Fonbook.cpp
@@ -238,7 +238,10 @@ void Fonbook::Save() {
 }
 
 size_t Fonbook::GetFonbookSize() {
-	return fonbookList.size();
+	if (initialized)
+		return fonbookList.size();
+	else
+		return 0;
 }
 
 void Fonbook::Sort(FonbookEntry::eElements element, bool ascending) {

commit 961915721e566707da500705dca10a345660c705

Fix log information about phone book size
- Fixes a problem introduced with 69a43bd7
  Fixes #577 at
  http://projects.vdr-developer.org/issues/

Fonbook.cpp
@@ -240,6 +240,12 @@ void Fonbook::Save() {
 	}
 }
 
+void Fonbook::setInitialized(bool isInitialized) {
+	initialized = isInitialized;
+	if (displayable && isInitialized)
+		INF(title << " initialized (" << GetFonbookSize() << " entries).");
+}
+
 size_t Fonbook::GetFonbookSize() const {
 	if (initialized)
 		return fonbookList.size();

Fonbook.h
@@ -247,7 +247,7 @@ public:
 	 * Sets the initialized-status.
 	 * @param isInititalized the value initialized is set to
 	 */
-	virtual void setInitialized(bool isInitialized) { initialized = isInitialized; }
+	virtual void setInitialized(bool isInitialized);
 	/**
 	 *  Returns the number of entries in the telephonebook.
 	 * @return the number of entries

FritzFonbook.cpp
@@ -147,7 +147,6 @@ void FritzFonbook::ParseHtmlFonbook(std::string *msg) {
 		}
 		AddFonbookEntry(fe);
 	}
-	INF("read " << GetFonbookSize() << " entries.");
 }
 
 void FritzFonbook::Reload() {

LocalFonbook.cpp
@@ -176,7 +176,6 @@ void LocalFonbook::ParseCsvFonbook(std::string filePath) {
 				ERR("parse error at " << s);
 			}
 		}
-		INF("read " << GetFonbookSize() << " entries.");
 		Sort(FonbookEntry::ELEM_NAME, true);
 		fclose(f);
 	}

XmlFonbook.cpp
@@ -230,7 +230,6 @@ void XmlFonbook::ParseXmlFonbook(std::string *msg) {
 		AddFonbookEntry(fe);
 		pos = msgConv.find("<contact>", pos+1);
 	}
-	INF("read " << GetFonbookSize() << " entries.");
 }
 
 std::string XmlFonbook::SerializeToXml() {